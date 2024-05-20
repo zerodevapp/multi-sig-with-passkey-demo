a2ee402e71afe941c36340dda8517cccfc39c5ba
Sahil Vasava <sahilvasava@gmail.com>
2024-05-21 06:00:59 +0700

    feat: add multi sig with async signing demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,266 +1,176 @@
-"use client"
-
+"use client";
+import { Inter } from "next/font/google";
+import { useState, useEffect } from "react";
 import {
-    createKernelAccount,
-    createKernelAccountClient,
-    createZeroDevPaymasterClient
-} from "@zerodev/sdk"
+  PASSKEY_URL,
+  createWeightedAccountClient,
+  entryPoint,
+  publicClient,
+  registerAndFetchPassKeyPublicKey,
+} from "./utils";
+import { KernelAccountClient, KernelSmartAccount } from "@zerodev/sdk";
 import {
-    createPasskeyValidator,
-    getPasskeyValidator
-} from "@zerodev/passkey-validator"
-import { bundlerActions, ENTRYPOINT_ADDRESS_V07 } from "permissionless"
-import React, { useEffect, useState } from "react"
-import { createPublicClient, http, parseAbi, encodeFunctionData } from "viem"
-import { sepolia } from "viem/chains"
+  WebAuthnKey,
+  WeightedKernelAccountClient,
+  encodeSignatures,
+  toECDSASigner,
+  toWebAuthnSigner,
+} from "@zerodev/weighted-validator";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { Hex, PrivateKeyAccount, Transport, Chain, zeroAddress } from "viem";
+import { ENTRYPOINT_ADDRESS_V07_TYPE } from "permissionless/types";
+import { bundlerActions } from "permissionless";
+import { encodeAbiParameters } from "viem/utils";
 
-const BUNDLER_URL =
-    "https://rpc.zerodev.app/api/v2/bundler/ec9a8985-9972-42d4-9879-15e21e4fe3b6"
-const PAYMASTER_URL =
-    "https://rpc.zerodev.app/api/v2/paymaster/ec9a8985-9972-42d4-9879-15e21e4fe3b6"
-const PASSKEY_SERVER_URL =
-    "https://passkeys.zerodev.app/api/v3/ec9a8985-9972-42d4-9879-15e21e4fe3b6"
-const CHAIN = sepolia
-
-const contractAddress = "0x34bE7f35132E97915633BC1fc020364EA5134863"
-const contractABI = parseAbi([
-    "function mint(address _to) public",
-    "function balanceOf(address owner) external view returns (uint256 balance)"
-])
-
-const publicClient = createPublicClient({
-    transport: http(BUNDLER_URL)
-})
-
-let kernelAccount: any
-let kernelClient: any
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    const [mounted, setMounted] = useState(false)
-    const [username, setUsername] = useState("")
-    const [accountAddress, setAccountAddress] = useState("")
-    const [isKernelClientReady, setIsKernelClientReady] = useState(false)
-    const [isRegistering, setIsRegistering] = useState(false)
-    const [isLoggingIn, setIsLoggingIn] = useState(false)
-    const [isSendingUserOp, setIsSendingUserOp] = useState(false)
-    const [userOpHash, setUserOpHash] = useState("")
-    const [userOpStatus, setUserOpStatus] = useState("")
+  const [passKeyName, setPassKeyName] = useState("");
+  const [publicKey, setPublicKey] = useState<WebAuthnKey>();
+  const [privateKey, setPrivateKey] = useState<Hex>();
+  const [signatures, setSignatures] = useState<Hex[]>([]);
+  const [signer, setSigner] = useState<PrivateKeyAccount>();
+  const [kernelClient, setKernelClient] =
+    useState<
+      WeightedKernelAccountClient<
+        ENTRYPOINT_ADDRESS_V07_TYPE,
+        Transport,
+        Chain,
+        KernelSmartAccount<ENTRYPOINT_ADDRESS_V07_TYPE>
+      >
+    >();
+  const [txHash, setTxHash] = useState<Hex>();
 
-    const createAccountAndClient = async (passkeyValidator: any) => {
-        kernelAccount = await createKernelAccount(publicClient, {
-            entryPoint: ENTRYPOINT_ADDRESS_V07,
-            plugins: {
-                sudo: passkeyValidator
-            }
-        })
+  useEffect(() => {
+    const pKey = generatePrivateKey();
+    setPrivateKey(pKey);
+    setSigner(privateKeyToAccount(pKey));
+  }, []);
 
-        console.log("Kernel account created: ", kernelAccount.address)
+  const createPassKey = async () => {
+    const _publicKey = await registerAndFetchPassKeyPublicKey(passKeyName);
+    setPublicKey(_publicKey);
+  };
 
-        kernelClient = createKernelAccountClient({
-            account: kernelAccount,
-            chain: CHAIN,
-            bundlerTransport: http(BUNDLER_URL),
-            entryPoint: ENTRYPOINT_ADDRESS_V07,
-            middleware: {
-                sponsorUserOperation: async ({ userOperation }) => {
-                    const zeroDevPaymaster = await createZeroDevPaymasterClient(
-                        {
-                            chain: CHAIN,
-                            transport: http(PAYMASTER_URL),
-                            entryPoint: ENTRYPOINT_ADDRESS_V07
-                        }
-                    )
-                    return zeroDevPaymaster.sponsorUserOperation({
-                        userOperation,
-                        entryPoint: ENTRYPOINT_ADDRESS_V07
-                    })
-                }
-            }
-        })
+  const createPassKeyWeightedAccount = async () => {
+    if (!signer || !publicKey) return;
+    const passKeySigner = await toWebAuthnSigner(publicClient, {
+      passkeyName: passKeyName,
+      passkeyServerUrl: PASSKEY_URL,
+      pubKey: publicKey,
+    });
+    const client = await createWeightedAccountClient(
+      passKeySigner,
+      signer?.address,
+      publicKey
+    );
+    console.log({ passKeyClient: client });
+    setKernelClient(client);
+  };
 
-        setIsKernelClientReady(true)
-        setAccountAddress(kernelAccount.address)
-    }
+  const createEcdsaWeightedAccount = async () => {
+    if (!signer || !publicKey) return;
+    const ecdsaSigner = await toECDSASigner({
+      signer,
+    });
+    const client = await createWeightedAccountClient(
+      ecdsaSigner,
+      signer?.address,
+      publicKey
+    );
+    console.log({ ecdsaClient: client });
+    setKernelClient(client);
+  };
 
-    // Function to be called when "Register" is clicked
-    const handleRegister = async () => {
-        setIsRegistering(true)
+  const approveUserOperation = async () => {
+    if (!kernelClient) return;
+    const signature = await kernelClient.approveUserOperation({
+      userOperation: {
+        callData: await kernelClient.account.encodeCallData({
+          to: zeroAddress,
+          data: "0x",
+          value: BigInt(0),
+        }),
+      },
+    });
+    console.log({ signature });
+    setSignatures([...signatures, signature]);
+  };
 
-        const passkeyValidator = await createPasskeyValidator(publicClient, {
-            passkeyName: username,
-            passkeyServerUrl: PASSKEY_SERVER_URL,
-            entryPoint: ENTRYPOINT_ADDRESS_V07
-        })
+  const sendTxWithClient = async () => {
+    if (!kernelClient) return;
+    const userOpHash = await kernelClient.sendUserOperation({
+      userOperation: {
+        callData: await kernelClient.account.encodeCallData({
+          to: zeroAddress,
+          value: BigInt(0),
+          data: "0x",
+        }),
+        signature: encodeSignatures(signatures),
+      },
+    });
+    console.log({ userOpHash });
+    const txReceipt = await kernelClient
+      .extend(bundlerActions(entryPoint))
+      .waitForUserOperationReceipt({ hash: userOpHash });
+    console.log({ txReceipt });
 
-        await createAccountAndClient(passkeyValidator)
+    setTxHash(txReceipt.receipt.transactionHash);
+  };
 
-        setIsRegistering(false)
-        window.alert("Register done.  Try sending UserOps.")
-    }
-
-    const handleLogin = async () => {
-        setIsLoggingIn(true)
-
-        const passkeyValidator = await getPasskeyValidator(publicClient, {
-            passkeyServerUrl: PASSKEY_SERVER_URL,
-            entryPoint: ENTRYPOINT_ADDRESS_V07
-        })
-
-        await createAccountAndClient(passkeyValidator)
-
-        setIsLoggingIn(false)
-        window.alert("Login done.  Try sending UserOps.")
-    }
-
-    // Function to be called when "Login" is clicked
-    const handleSendUserOp = async () => {
-        setIsSendingUserOp(true)
-        setUserOpStatus("Sending UserOp...")
-
-        const userOpHash = await kernelClient.sendUserOperation({
-            userOperation: {
-                callData: await kernelAccount.encodeCallData({
-                    // @dev kernel v3 does not have onErc721Received, so it will fail
-                    // to: contractAddress,
-                    // value: BigInt(0),
-                    // data: encodeFunctionData({
-                    //     abi: contractABI,
-                    //     functionName: "mint",
-                    //     args: [kernelAccount.address]
-                    // })
-                    // @dev instead we just send a simple UserOp
-                    to: "0x0000000000000000000000000000000000000000",
-                    value: BigInt(0),
-                    data: "0x"
-                })
-            }
-        })
-
-        setUserOpHash(userOpHash)
-
-        const bundlerClient = kernelClient.extend(
-            bundlerActions(ENTRYPOINT_ADDRESS_V07)
-        )
-        await bundlerClient.waitForUserOperationReceipt({
-            hash: userOpHash
-        })
-
-        // Update the message based on the count of UserOps
-        const userOpMessage = `UserOp completed. <a href="https://jiffyscan.xyz/userOpHash/${userOpHash}?network=mumbai" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:text-blue-700">Click here to view.</a>`
-
-        setUserOpStatus(userOpMessage)
-        setIsSendingUserOp(false)
-    }
-
-    useEffect(() => {
-        setMounted(true)
-    }, [])
-
-    if (!mounted) return <></>
-
-    // Spinner component for visual feedback during loading states
-    const Spinner = () => (
-        <svg
-            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-        >
-            <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-            ></circle>
-            <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-        </svg>
-    )
-
-    return (
-        <main className="flex items-center justify-center min-h-screen px-4 py-24">
-            <div className="w-full max-w-lg mx-auto">
-                <h1 className="text-4xl font-semibold text-center mb-12">
-                    ZeroDev Passkeys Demo
-                </h1>
-
-                <div className="space-y-4">
-                    {/* Account Address Label */}
-                    {accountAddress && (
-                        <div className="text-center mb-4">
-                            Account address:{" "}
-                            <a
-                                href={`https://jiffyscan.xyz/account/${accountAddress}`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-blue-500 hover:text-blue-700"
-                            >
-                                {" "}
-                                {accountAddress}{" "}
-                            </a>
-                        </div>
-                    )}
-
-                    {/* Input Box */}
-                    <input
-                        type="text"
-                        placeholder="Your username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        className="p-2 border border-gray-300 rounded-lg w-full"
-                    />
-
-                    {/* Register and Login Buttons */}
-                    <div className="flex flex-col sm:flex-row sm:space-x-4">
-                        {/* Register Button */}
-                        <button
-                            onClick={handleRegister}
-                            disabled={isRegistering || isLoggingIn}
-                            className="flex justify-center items-center px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 w-full"
-                        >
-                            {isRegistering ? <Spinner /> : "Register"}
-                        </button>
-
-                        {/* Login Button */}
-                        <button
-                            onClick={handleLogin}
-                            disabled={isLoggingIn || isRegistering}
-                            className="mt-2 sm:mt-0 flex justify-center items-center px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 w-full"
-                        >
-                            {isLoggingIn ? <Spinner /> : "Login"}
-                        </button>
-                    </div>
-
-                    {/* Send UserOp Button */}
-                    <div className="flex flex-col items-center w-full">
-                        <button
-                            onClick={handleSendUserOp}
-                            disabled={!isKernelClientReady || isSendingUserOp}
-                            className={`px-4 py-2 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-opacity-50 flex justify-center items-center w-full ${
-                                isKernelClientReady && !isSendingUserOp
-                                    ? "bg-green-500 hover:bg-green-700 focus:ring-green-500"
-                                    : "bg-gray-500"
-                            }`}
-                        >
-                            {isSendingUserOp ? <Spinner /> : "Send UserOp"}
-                        </button>
-                        {/* UserOp Status Label */}
-                        {userOpHash && (
-                            <div
-                                className="mt-4"
-                                dangerouslySetInnerHTML={{
-                                    __html: userOpStatus
-                                }}
-                            />
-                        )}
-                    </div>
-                </div>
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="flex flex-col">
+          <div className="flex flex-col">
+            <div>{`EOA Address:  ${signer?.address}`}</div>
+            <div>{`PrivateKey:  ${privateKey}`}</div>
+            <div>{`PassKeyName:  ${passKeyName}`}</div>
+            <div> {`PassKeyPublicKey-X:  ${publicKey?.pubX}`}</div>
+            <div> {`PassKeyPublicKey-Y:  ${publicKey?.pubY}`}</div>
+            <div>{`PassKeyPublicKey-authenticatorIdHash:  ${publicKey?.authenticatorIdHash}`}</div>
+            <div className="max-w-5xl">{`signatures:  ${signatures}`}</div>
+            <div>{`txHash:  ${txHash}`}</div>
+          </div>
+          <div className="flex flex-col">
+            <div>
+              <input
+                type="text"
+                placeholder="PassKey Name"
+                value={passKeyName}
+                onChange={(e) => setPassKeyName(e.target.value)}
+                className="p-2 border border-gray-300 rounded-lg w-full"
+              />
+              <button onClick={createPassKey}>Create Passkey</button>
             </div>
-        </main>
-    )
+            <div>
+              <button onClick={createPassKeyWeightedAccount}>
+                Create Passkey Client
+              </button>
+            </div>
+            <div>
+              <button onClick={approveUserOperation}>
+                Approve userOperation with PassKey
+              </button>
+            </div>
+            <div>
+              <button onClick={createEcdsaWeightedAccount}>
+                Create Ecdsa Client
+              </button>
+            </div>
+            <div>
+              <button onClick={approveUserOperation}>
+                Approve userOperation with Ecdsa
+              </button>
+            </div>
+            <div>
+              <button onClick={sendTxWithClient}>Send Tx with Client</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ app/utils.ts
@@ -0,0 +1,90 @@
+import {
+  toWebAuthnPubKey,
+  toWebAuthnSigner,
+  toECDSASigner,
+  WebAuthnMode,
+  WebAuthnKey,
+  createWeightedValidator,
+  SIGNER_TYPE,
+  createWeightedKernelAccountClient,
+} from "@zerodev/weighted-validator";
+import {
+  createKernelAccount,
+  createZeroDevPaymasterClient,
+  createKernelAccountClient,
+} from "@zerodev/sdk";
+import { Address, PrivateKeyAccount, createPublicClient, http } from "viem";
+import { sepolia } from "viem/chains";
+import { ENTRYPOINT_ADDRESS_V07 } from "permissionless";
+import { WeightedSigner } from "@zerodev/weighted-validator";
+
+export const PASSKEY_URL =
+  "https://passkeys.zerodev.app/api/v3/efbc1add-1c14-476e-b3f1-206db80e673c";
+export const BUNDLER_URL =
+  "https://rpc.zerodev.app/api/v2/bundler/efbc1add-1c14-476e-b3f1-206db80e673c";
+export const PAYMASTER_URL =
+  "https://rpc.zerodev.app/api/v2/paymaster/efbc1add-1c14-476e-b3f1-206db80e673c";
+export const entryPoint = ENTRYPOINT_ADDRESS_V07;
+export const chain = sepolia;
+export const publicClient = createPublicClient({
+  transport: http(BUNDLER_URL),
+  chain,
+});
+
+export const registerAndFetchPassKeyPublicKey = async (
+  passkeyName: string
+): Promise<WebAuthnKey> => {
+  return await toWebAuthnPubKey({
+    passkeyName,
+    passkeyServerUrl: PASSKEY_URL,
+    mode: WebAuthnMode.Register,
+  });
+};
+
+export const createWeightedAccountClient = async (
+  signer: WeightedSigner,
+  ecdsaSignerAddress: Address,
+  publicKey: WebAuthnKey
+) => {
+  const multiSigValidator = await createWeightedValidator(publicClient, {
+    entryPoint,
+    signer,
+    config: {
+      threshold: 100,
+      signers: [
+        {
+          publicKey: ecdsaSignerAddress,
+          weight: 50,
+        },
+        {
+          publicKey,
+          weight: 100,
+        },
+      ],
+    },
+  });
+
+  const account = await createKernelAccount(publicClient, {
+    entryPoint,
+    plugins: {
+      sudo: multiSigValidator,
+    },
+  });
+
+  const paymasterClient = createZeroDevPaymasterClient({
+    entryPoint,
+    chain,
+    transport: http(PAYMASTER_URL),
+  });
+
+  const client = createWeightedKernelAccountClient({
+    account,
+    entryPoint,
+    chain,
+    bundlerTransport: http(BUNDLER_URL),
+    middleware: {
+      sponsorUserOperation: paymasterClient.sponsorUserOperation,
+    },
+  });
+  return client;
+};
